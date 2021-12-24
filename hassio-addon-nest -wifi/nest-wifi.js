b47e02d221ac1270f4a66f7289d971130bcc8421
Nicolas Inchauspe <nicolas.inchauspe@gmail.com>
2021-12-24 17:44:59 +0100

    :recycle: Refactor functions

--- hassio-addon-nest -wifi/nest-wifi.js
+++ hassio-addon-nest -wifi/nest-wifi.js
@@ -2,14 +2,22 @@ const GoogleWifiApi = require('/usr/src/app/node_modules/google-wifi-api-node/in
 const axios = require('/usr/src/app/node_modules/axios');
 const cron = require('/usr/src/app/node_modules/node-cron');
 
-async function getInfos(googleWifiApi) {
-  const groups = await googleWifiApi.getGroups();
-  const groupsStatus = await googleWifiApi.getGroupStatus(groups.groups[0].id);
-  groups.groups[0].accessPoints.forEach(accessPoint => {
-    accessPoint.apState = groupsStatus.apStatuses.find(
-      apStatus => apStatus.apId === accessPoint.id,
-    )?.apState;
-  });
+async function getAccessPointsInfo(googleWifiApi) {
+  let groups;
+  try {
+    groups = await googleWifiApi.getGroups();
+    const groupsStatus = await googleWifiApi.getGroupStatus(
+      groups.groups[0].id,
+    );
+    groups.groups[0].accessPoints.forEach(accessPoint => {
+      accessPoint.apState = groupsStatus.apStatuses.find(
+        apStatus => apStatus.apId === accessPoint.id,
+      )?.apState;
+    });
+  } catch (error) {
+    console.error('Could not get access points infos', error);
+  }
+
   try {
     await axios.post(
       'http://supervisor/core/api/states/sensor.nest_wifi_access_points',
@@ -30,7 +38,17 @@ async function getInfos(googleWifiApi) {
     process.exit(1);
   }
 
-  const devices = await googleWifiApi.getGroupDevices(groups.groups[0].id);
+  return groups;
+}
+
+async function getDevicesInfo(googleWifiApi, groups) {
+  let devices;
+  try {
+    devices = await googleWifiApi.getGroupDevices(groups.groups[0].id);
+  } catch (error) {
+    console.error('Could not get devices infos', error);
+  }
+
   try {
     await axios.post(
       'http://supervisor/core/api/states/sensor.nest_wifi_devices',
@@ -50,10 +68,18 @@ async function getInfos(googleWifiApi) {
     );
     process.exit(1);
   }
+}
 
-  const speedTestResults = await googleWifiApi.getGroupSpeedTestResults(
-    groups.groups[0].id,
-  );
+async function getSpeedTestResults(googleWifiApi, groups) {
+  let speedTestResults;
+  try {
+    speedTestResults = await googleWifiApi.getGroupSpeedTestResults(
+      groups.groups[0].id,
+    );
+  } catch (error) {
+    console.error('Could not get speed test results', error);
+  }
+
   try {
     await axios.post(
       'http://supervisor/core/api/states/sensor.nest_wifi_speed_test_results',
@@ -76,6 +102,12 @@ async function getInfos(googleWifiApi) {
     );
     process.exit(1);
   }
+}
+
+async function getInfos(googleWifiApi) {
+  const groups = await getAccessPointsInfo(googleWifiApi);
+  await getDevicesInfo(googleWifiApi, groups);
+  await getSpeedTestResults(googleWifiApi, groups);
 }
 
 module.exports = async options => {
