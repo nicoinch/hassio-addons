4ae93c621c315963a179b1ee9471781e44cc6b73
Nicolas Inchauspe <nicolas.inchauspe@gmail.com>
2021-12-24 14:55:43 +0100

    :sparkles: Add speed test results

--- hassio-addon-nest -wifi/nest-wifi.js
+++ hassio-addon-nest -wifi/nest-wifi.js
@@ -21,9 +21,12 @@ async function getInfos(googleWifiApi) {
         headers: { Authorization: 'Bearer ' + process.env.SUPERVISOR_TOKEN },
       },
     );
-    console.log('Posted Nest Wifi to Hass');
+    console.log('Posted Nest Wifi Access Points to Hass');
   } catch (error) {
-    console.log('Could not send sensor status to Home Assistant', error);
+    console.log(
+      'Could not send Access Points sensor status to Home Assistant',
+      error,
+    );
     process.exit(1);
   }
 
@@ -39,9 +42,38 @@ async function getInfos(googleWifiApi) {
         headers: { Authorization: 'Bearer ' + process.env.SUPERVISOR_TOKEN },
       },
     );
-    console.log('Posted Nest Wifi to Hass');
+    console.log('Posted Nest Wifi Devices to Hass');
   } catch (error) {
-    console.log('Could not send sensor status to Home Assistant', error);
+    console.log(
+      'Could not send Devices sensor status to Home Assistant',
+      error,
+    );
+    process.exit(1);
+  }
+
+  const speedTestResults = await googleWifiApi.getGroupSpeedTestResults(
+    groups.groups[0].id,
+  );
+  try {
+    await axios.post(
+      'http://supervisor/core/api/states/sensor.nest_wifi_speed_test_results',
+      {
+        state: Math.round(
+          parseInt(speedTestResults.speedTestResults[0].transmitWanSpeedBps) /
+            1000000,
+        ),
+        attributes: { data: speedTestResults.speedTestResults[0] },
+      },
+      {
+        headers: { Authorization: 'Bearer ' + process.env.SUPERVISOR_TOKEN },
+      },
+    );
+    console.log('Posted Nest Wifi Speed Test Results to Hass');
+  } catch (error) {
+    console.log(
+      'Could not send Speed Test Results sensor status to Home Assistant',
+      error,
+    );
     process.exit(1);
   }
 }
