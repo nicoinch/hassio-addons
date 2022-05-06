64056c2e8825d90772d93768088e66c4f2ff047e
Nicolas Inchauspe <nicolas.inchauspe@gmail.com>
2022-05-06 17:47:20 +0200

    :bug: Fix non functional code

--- hassio-addon-nest -wifi/nest-wifi.js
+++ hassio-addon-nest -wifi/nest-wifi.js
@@ -1,43 +1,48 @@
-const GoogleWifiApi = require('/usr/src/app/node_modules/google-wifi-api-node/index');
-const axios = require('/usr/src/app/node_modules/axios');
-const cron = require('/usr/src/app/node_modules/node-cron');
+const GoogleWifiApi = require("/usr/src/app/node_modules/google-wifi-api-node/index");
+const axios = require("/usr/src/app/node_modules/axios");
+const cron = require("/usr/src/app/node_modules/node-cron");
 
 async function getAccessPointsInfo(googleWifiApi) {
   let groups;
   try {
     groups = await googleWifiApi.getGroups();
     const groupsStatus = await googleWifiApi.getGroupStatus(
-      groups.groups[0].id,
+      groups.groups[0].id
     );
-    groups.groups[0].accessPoints.forEach(accessPoint => {
+    groups.groups[0].accessPoints.forEach((accessPoint) => {
       accessPoint.apState = groupsStatus.apStatuses.find(
-        apStatus => apStatus.apId === accessPoint.id,
+        (apStatus) => apStatus.apId === accessPoint.id
       )?.apState;
     });
   } catch (error) {
-    console.error('Could not get access points infos', error);
+    console.error("Could not get access points infos", error);
   }
 
-    groups.groups[0].accessPoints.forEach(accessPoint => {
-    try {await axios.post(
-      'http://supervisor/core/api/states/sensor.nest_wifi_access_points_' + accessPoint.accessPointSettings.accessPointOtherSettings.apName.toLowerCase().replace(/\s/g, '_'),
-      {
-        state: accessPoint.apState,
-        attributes: { data: accessPoint },
-      },
-      {
-        headers: { Authorization: 'Bearer ' + process.env.SUPERVISOR_TOKEN },
-      },
-    );
-    console.log('Posted Nest Wifi Access Points to Hass');
-  } catch (error) {
-    console.log(
-      'Could not send Access Points sensor status to Home Assistant',
-      error,
-    );
-    process.exit(1);
-  }});
-    
+  groups.groups[0].accessPoints.forEach(async (accessPoint) => {
+    try {
+      await axios.post(
+        "http://supervisor/core/api/states/sensor.nest_wifi_access_points_" +
+          accessPoint.accessPointSettings.accessPointOtherSettings.apName
+            .toLowerCase()
+            .replace(/\s/g, "_"),
+        {
+          state: accessPoint.apState,
+          attributes: { data: accessPoint },
+        },
+        {
+          headers: { Authorization: "Bearer " + process.env.SUPERVISOR_TOKEN },
+        }
+      );
+      console.log("Posted Nest Wifi Access Points to Hass");
+    } catch (error) {
+      console.log(
+        "Could not send Access Points sensor status to Home Assistant",
+        error
+      );
+      process.exit(1);
+    }
+  });
+
   return groups;
 }
 
@@ -46,25 +51,25 @@ async function getDevicesInfo(googleWifiApi, groups) {
   try {
     devices = await googleWifiApi.getGroupDevices(groups.groups[0].id);
   } catch (error) {
-    console.error('Could not get devices infos', error);
+    console.error("Could not get devices infos", error);
   }
 
   try {
     await axios.post(
-      'http://supervisor/core/api/states/sensor.nest_wifi_devices',
+      "http://supervisor/core/api/states/sensor.nest_wifi_devices",
       {
         state: devices.stations.length,
         attributes: { data: devices.stations },
       },
       {
-        headers: { Authorization: 'Bearer ' + process.env.SUPERVISOR_TOKEN },
-      },
+        headers: { Authorization: "Bearer " + process.env.SUPERVISOR_TOKEN },
+      }
     );
-    console.log('Posted Nest Wifi Devices to Hass');
+    console.log("Posted Nest Wifi Devices to Hass");
   } catch (error) {
     console.log(
-      'Could not send Devices sensor status to Home Assistant',
-      error,
+      "Could not send Devices sensor status to Home Assistant",
+      error
     );
     process.exit(1);
   }
@@ -74,31 +79,44 @@ async function getSpeedTestResults(googleWifiApi, groups) {
   let speedTestResults;
   try {
     speedTestResults = await googleWifiApi.getGroupSpeedTestResults(
-      groups.groups[0].id,
+      groups.groups[0].id
     );
   } catch (error) {
-    console.error('Could not get speed test results', error);
+    console.error("Could not get speed test results", error);
   }
 
   try {
     await axios.post(
-      'http://supervisor/core/api/states/sensor.nest_wifi_speed_test_results',
+      "http://supervisor/core/api/states/sensor.nest_wifi_download_speed",
       {
         state: Math.round(
-          parseInt(speedTestResults.speedTestResults[0].transmitWanSpeedBps) /
-            1000000,
+          parseInt(speedTestResults.speedTestResults[0].receiveWanSpeedBps) /
+            1000000
         ),
         attributes: { data: speedTestResults.speedTestResults[0] },
       },
       {
-        headers: { Authorization: 'Bearer ' + process.env.SUPERVISOR_TOKEN },
-      },
+        headers: { Authorization: "Bearer " + process.env.SUPERVISOR_TOKEN },
+      }
     );
-    console.log('Posted Nest Wifi Speed Test Results to Hass');
+    await axios.post(
+      "http://supervisor/core/api/states/sensor.nest_wifi_upload_speed",
+      {
+        state: Math.round(
+          parseInt(speedTestResults.speedTestResults[0].transmitWanSpeedBps) /
+            1000000
+        ),
+        attributes: { data: speedTestResults.speedTestResults[0] },
+      },
+      {
+        headers: { Authorization: "Bearer " + process.env.SUPERVISOR_TOKEN },
+      }
+    );
+    console.log("Posted Nest Wifi Speed Test Results to Hass");
   } catch (error) {
     console.log(
-      'Could not send Speed Test Results sensor status to Home Assistant',
-      error,
+      "Could not send Speed Test Results sensor status to Home Assistant",
+      error
     );
     process.exit(1);
   }
@@ -110,16 +128,16 @@ async function getInfos(googleWifiApi) {
   await getSpeedTestResults(googleWifiApi, groups);
 }
 
-module.exports = async options => {
+module.exports = async (options) => {
   if (!options.googleWifiApiKey) {
-    console.error('No options.googleWifiApiKey provided');
+    console.error("No options.googleWifiApiKey provided");
     return;
   }
   const googleWifiApi = new GoogleWifiApi(options.googleWifiApiKey);
   await googleWifiApi.init();
 
-  cron.schedule('* * * * *', () => {
+  cron.schedule("* * * * *", () => {
     getInfos(googleWifiApi);
-    console.log('getInfos cronjob executed at: ' + new Date());
+    console.log("getInfos cronjob executed at: " + new Date());
   });
 };
